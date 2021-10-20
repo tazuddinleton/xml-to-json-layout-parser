b385deb4ccecfefa2ba20d0fe80c2d40d9654874
Taz Uddin <getleton@gmail.com>
2021-10-21 04:40:28 +0600

    Organize project structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 import { writeFileSync, readFileSync } from 'fs';
 import {mapSection, addNr} from './lib.js'
 
-const data = JSON.parse(readFileSync('./input.json'));
-console.log('data loaded.')
+const data = JSON.parse(readFileSync('./input/data.json'));
+if(!data.length){
+  console.error('data not loaded');  
+}else{
+  console.log('data loaded.')
+}
 console.log('start processing...')
 let config = {
   dac6: {
@@ -14,5 +18,7 @@ let config = {
   }
 }
 
-writeFileSync('output.json', JSON.stringify(config));
+
+console.log('writing output...');
+writeFileSync('./output/config.json', JSON.stringify(config));
 console.log('done.')
